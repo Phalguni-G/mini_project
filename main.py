8e8285c782bb61b594a365df33dd71e5517337d5
Phalguni <106329125+Phalguni-G@users.noreply.github.com>
2024-02-01 11:52:54 +0530

    Make main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import sys
+
+def add(a,b):
+    return a+b
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a, b = sys.argv[1], sys.argv[2]
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
