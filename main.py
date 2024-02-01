423f77eb4d7dfcd6ebdec621078436efb20c78ad
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-02-01 12:16:27 +0530

    comit

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ import sys
 def add(a,b):
     return a+b
 
+def sub(a,b):
+    return a-b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     a, b = sys.argv[1], sys.argv[2]
+    print(a,b)
+    print(add(a,b))
+    print(sub(a,b))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
